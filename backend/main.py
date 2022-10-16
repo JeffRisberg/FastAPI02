4ea54d7002aff43d32308c62c06171e9549a8f68
JeffRisberg <j.s.risberg@gmail.com>
2022-10-15 19:01:46 -0700

    reorganized this example

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,5 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import requests
-
-origins = ["http://localhost:1234"]
+import uvicorn
 
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/users")
-def users():
-    resp = requests.get("https://reqres.in/api/users")
-    # TODO Discuss how to handle errors
-    return resp.json().get("data")
-
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    resp = requests.get(f"https://reqres.in/api/users/{user_id}")
-    # TODO Discuss how to handle errors
-    return resp.json().get("data")
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
