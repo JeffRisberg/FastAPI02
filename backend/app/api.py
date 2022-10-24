efda4b3f17c725e829e9dd6a24de1eec3c3cbca2
JeffRisberg <j.s.risberg@gmail.com>
2022-10-24 16:41:27 -0700

    added delete method

--- backend/app/api.py
+++ backend/app/api.py
@@ -54,6 +54,19 @@ async def update_todo(id: int, body: dict) -> dict:
     "data": f"Todo with id {id} not found."
   }
 
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+  for todo in todos:
+    if int(todo["id"]) == id:
+      todos.remove(todo)
+      return {
+        "data": f"Todo with id {id} has been removed."
+      }
+
+  return {
+    "data": f"Todo with id {id} not found."
+  }
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
