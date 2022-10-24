e588b0a7b52562f3aea4683c11eb4982181c93b3
JeffRisberg <j.s.risberg@gmail.com>
2022-10-24 16:33:36 -0700

    added put method

--- backend/app/api.py
+++ backend/app/api.py
@@ -41,6 +41,19 @@ async def add_todo(todo: dict) -> dict:
     "data": { "Todo added." }
   }
 
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+  for todo in todos:
+    if int(todo["id"]) == id:
+      todo["item"] = body["item"]
+      return {
+        "data": f"Todo with id {id} has been updated."
+      }
+
+  return {
+    "data": f"Todo with id {id} not found."
+  }
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
